45ea5d1ec4a2fca2e12d394a4fa429e620233641
Stephen Tedesco <s_tylert@icloud.com>
2022-02-19 11:36:22 -0700

    Adds new recipes to arrays in foods.js

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
+//~~~~~~~~~~~~~~~~~~~~Variables~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 var letsCookBtn = document.querySelector(".letsCook");
 var addRecipe = document.querySelector(".addRecipe");
 var side = document.getElementById("side");
 var mainDish = document.getElementById("mainDish");
 var dessert = document.getElementById("dessert");
 var entireMeal = document.getElementById("entireMeal");
-var crockPot = document.querySelector(".centered")
-var recipeForm = document.querySelector(".addRecipeSection")
+var crockPot = document.querySelector(".centered");
+var recipeForm = document.querySelector(".addRecipeSection");
+var addNewButton = document.querySelector(".addNewButton");
+var newRecipeType = document.getElementById("recipeType");
+var newRecipeName = document.getElementById("recipeName");
+var allDishes ={
+  sides: sides,
+  mains: mains,
+  desserts: desserts,
+  entireMeal: [sides, mains, desserts]
+};
 
+//~~~~~~~~~~~~~~~~Event Listeners~~~~~~~~~~~~~~~~~~~~~~~~//
 letsCookBtn.addEventListener("click", showDish);
-addRecipe.addEventListener("click", function(){
-  recipeForm.classList.remove("hidden")
-})
+addRecipe.addEventListener("click", addNewRecipe)
+addNewButton.addEventListener("click", addNewRecipe)
+
+//~~~~~~~~~~~~~~~~~~~~Functions~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+function hide(element){
+  element.classList.a("hidden")
+}
+
+function show(element){
+  element.classList.remove("hidden")
+}
 
 function randomDish(array){
   return array[Math.floor(Math.random() * array.length)];
@@ -19,12 +38,6 @@ function randomDish(array){
 function showDish(){
   var dishSelected = document.querySelector('input[name="dishOption"]:checked');
   var clickedBox = dishSelected.value;
-  var allDishes ={
-    sides: sides,
-    mains: mains,
-    desserts: desserts,
-    entireMeal: [sides, mains, desserts]
-  }
   var dishToDisplay = randomDish(allDishes[clickedBox])
   if(clickedBox === "entireMeal"){
     crockPot.innerHTML = `
@@ -39,4 +52,14 @@ function showDish(){
      <p class="recipeText">${dishToDisplay}</p>
      </div>`;
    }
+}
+
+function addNewRecipe(){
+  show(recipeForm)
+  event.preventDefault();
+  if (!allDishes[newRecipeType.value]){
+     allDishes[newRecipeType.value] = [];
+     allDishes[newRecipeType.value].unshift(newRecipeName.value)
+  }
+  allDishes[newRecipeType.value].unshift(newRecipeName.value)
 }
